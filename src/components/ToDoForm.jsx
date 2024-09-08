c65c9f70dfbeae68367f307e993cab70bda8aeed
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2024-09-08 15:07:15 +0330

    style ,state, handleSubmit & handleChange added!

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const ToDoForm = () => {
+    const [value, setValue] = useState('');
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        console.log(value);
+
+    }
+
     return (
-        <div>ToDoForm</div>
+        <form on onSubmit={handleSubmit}>
+            <input type="text" className='p-3 rounded-md bg-gray-200 hover:bg-white outline-none' onChange={handleChange} placeholder='what is task today?' />
+            <button type='submit' className='bg-slate-600 text-white rounded-md ml-5 p-2'>Add</button>
+        </form>
     )
+
 }
+
